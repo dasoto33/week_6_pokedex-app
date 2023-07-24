59c968ef6f445f174636909938b2a8599e2f69d5
David Soto <daslife33@gmail.com>
2023-07-24 03:42:56 -0600

    checkpoint

--- app/models.py
+++ app/models.py
@@ -1,12 +1,14 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class Trainer(db.Model):
-    trainer_id = db.Column(db.Integer, primary_key=True)
+    trainer_id = db.Column(db.Integer, primary_key = True)
     first_name = db.Column(db.String(15))
-    trainer_name = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(70), nullable=False)
-    password_hash = db.Column(db.String(), nullable=False)
-    poke_relate = db.relationship('Pokemon', backref='owner', lazy=True)
+    trainer_name = db.Column(db.String(20), nullable = False, unique = True)
+    email = db.Column(db.String(70), nullable = False, unique = True)
+    password_hash = db.Column(db.String(), nullable = False)
+    poke_relate = db.relationship('Pokemon', backref = 'owner', lazy = True)
 
     def __repr__(self):
         return f'<TRAINER: {self.trainer_name}'
@@ -15,6 +17,11 @@ class Trainer(db.Model):
         db.session.add(self)
         db.session.commit()
                            
+    def hash_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Pokemon(db.Model):
     pokemon_id = db.Column(db.Integer, primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm, RegisterForm
-
+from app.models import Trainer
 
 
 @app.route('/')
@@ -18,8 +18,13 @@ def home():
 def sign_in():
    login_form = LoginForm()
    if login_form.validate_on_submit():
-       flash(f'{login_form.email.data} logged in!', category='success')
-       return redirect('/')
+       email = login_form.email.data
+       trainer = Trainer.query.filter_by(email=email).first()
+       if trainer and trainer.check_password(login_form.password.data):
+            flash(f'{login_form.email.data} logged in!', category='success')
+            return redirect('/')
+       else:
+           flash(f'Invalid login, please try again.', category='warning')
    return render_template('signin.jinja', form=login_form)
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -28,6 +33,11 @@ def register():
     if form.validate_on_submit():
         first_name = form.first_name.data
         trainer_name = form.trainer_name.data
+        email = form.email.data
+        password = form.password.data
+        trainer = Trainer(first_name=first_name, trainer_name=trainer_name, email=email)
+        trainer.hash_password(form.password.data)
+        trainer.commit()
         flash(f'{first_name if first_name else trainer_name} registered', category='success')
         return redirect('/')
     return render_template('register.jinja', form=form)
